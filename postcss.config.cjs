3f622da8e89e39ffab55b0aa08350084247e4e4f
MoralesMortales <carlos52.7624@gmail.com>
2025-05-29 21:29:11 -0500

    added Tailwind

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},  // Reemplaza 'tailwindcss' por esto
+    autoprefixer: {},
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('mainView');
 });
 Route::get('/hola', function () {
     return view('hola');
