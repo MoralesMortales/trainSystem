8f004d9fa15ffd5e07d4377ca43950511b8f3e52
MoralesMortales <moralesyaguilera@gmail.com>
2025-06-02 17:44:22 -0400

    imAgenius

--- postcss.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},  // Reemplaza 'tailwindcss' por esto
-    autoprefixer: {},
-  }
-}
--- resources/views/mainView.blade.php
+++ resources/views/mainView.blade.php
@@ -1,42 +1,16 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!doctype html>
+<html>
 
-        <title>TrainSystem</title>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+</head>
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Konkhmer+Sleokchher&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+<body>
+    <h1 class="tw:text-3xl  tw:text-red-300 font-bold underline">
+        Hello world!
+    </h1>
+</body>
 
-@vite(['resources/css/app.css'])
-@vite(['resources/css/mainView.css'])
-
-    </head>
-
-    <body>
-
-    <div id="container">
-
-    <x-navbar/>
-<div class="titles h-full flex flex-col justify-center pt-40 pl-20 gap-14">
-
-    <h2 class="title text-6xl">
-        Book Your Next Travel
-    </h2>
-
-    <h4 class="title text-5xl">
-    Book Now!
-    </h4>
-        @auth
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquam aut, corrupti eius doloribus dolore repudiandae repellendus qui nesciunt debitis veniam mollitia, cum voluptatem odit omnis, repellat nisi possimus deleniti.
-    @endauth
-
-</div>
-    </div>
-
-    </body>
 </html>
