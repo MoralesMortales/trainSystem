b95aa8713ce16fe6734e27e5c4e38319323e2252
MoralesMortales <carlos52.7624@gmail.com>
2025-06-10 12:15:02 -0500

    2db1left

---
+++ database/migrations/0004_reservation_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id('reservingNumber');
+            $table->string('travelCode');
+            $table->string('fullname');
+            $table->unsignedBigInteger('cedula');
+            $table->string('gender');
+            $table->string('passportNumber')->unique();
+
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+
+            $table->foreign('travelCode')->references('travelCode')->on('travels');
+            $table->foreign('cedula')->references('cedula')->on('users');
+
+            $table->index(['travelCode', 'cedula']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
